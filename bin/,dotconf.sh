96ba519be2b537de4574426a345da6949e78686c
raphigaziano <r.gaziano@gmail.com>
2013-05-21 15:12:16 +0200

    bon/,dotconf.sh - Add -d flag + better USAGE

--- bin/,dotconf.sh
+++ bin/,dotconf.sh
@@ -7,20 +7,26 @@
 #
 # Otherwise, create a new config file and it's symlink in $HOME.
 # 
-# TODO: Add -d flag to create a directory
+# Passing the -d flag as first argument will create a config directory.
 #
 # raphigaziano <r.gaziano@gmail.com>
 # Created: 05-20-2013
 #####################################################
 
-USAGE=",dotconf [-d] <filename>[, <filename>...]"
+USAGE="`basename $0` [-d] <filename>[, <filename>...]"
 CONFDIR=~/.dotfiles
-
+echo $USAGE; exit
 # Check args
 if [ $# -lt 1  ]
 then
     echo "Usage: $USAGE"
     exit 1
+fi
+
+# Naive flag detection
+if [[ $1 == -d  ]]; then
+    diropt=1
+    shift
 fi
 
 for filename in $*
@@ -29,8 +35,12 @@ do
     # Create files in ~/.dotfiles along with symlinks in $HOME
     if [ ! -e $path ]; then
         echo "create $path"
-        touch $path
-        echo new link ~/.${filename} to ${path}
+        if [ -z $diropt ]; then     # Regular file
+            touch $path
+        else
+            mkdir $path             # Directory
+        fi
+        echo "new link ~/.${filename} -> ${path}"
         ln -s $path ~/.$filename
     fi
 done
