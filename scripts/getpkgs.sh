e82c6bf7dab906a7dd2a896cb60dcee48155effd
raphigaziano <r.gaziano@gmail.com>
2013-05-22 12:28:19 +0200

    Added pkgs installation scripts

---
+++ scripts/getpkgs.sh
@@ -0,0 +1,36 @@
+# ! /bin/bash
+#
+# Automagically install the listed packages.
+# Update the package list below accordingly.
+#
+# Should run on any Debian-like system (needs apt-get)
+#
+# raphigaziano <r.gaziano@gmail.com>
+# Created: 05-22-2013
+#
+##############################################################################
+
+packages=(
+    tmux
+    tree 
+    python-pip
+)
+
+# Check if user is root
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+fi
+
+echo "Updating package index..."; echo
+apt-get update
+echo "Done."
+
+for pname in ${packages[@]}
+do
+    echo "Installing $pname..."
+    apt-get install $pname
+    echo "Done."
+done
+
+exit 0
---
+++ scripts/pypkgs.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+#
+# Install python packages listed in pip-requirements.
+#
+# Dependencies:
+# - Pip itself should already be installed.
+# - curl (not installed by default on some systems)
+#
+# raphigaziano <r.gaziano@gmail.com>
+# Created: 05-22-2013
+#
+##############################################################################
+
+# Check if user is root
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root"
+    exit 1
+fi
+
+CONFIR=~/.dotfiles
+BURRITOURL=https://raw.github.com/brainsik/virtualenv-burrito/master/virtualenv-burrito.sh
+
+# First get virtualenv & virtualenvwrapper, 
+# using venv-burrito to simplify the process
+curl -s $BURRITOURL | $SHELL
+
+# Create a "master" virtual env ?
+# source $VENVBURRITO/startup.sh
+
+# Install packages from pip-requirements.txt
+# (pip should be installed already)
+pip install -r $CONFDIR/scripts/pip-requirements.txt
