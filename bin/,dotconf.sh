d0a846bba2358bc903b5fe7595d693bd3977fa26
raphigaziano <r.gaziano@gmail.com>
2013-05-31 09:47:24 +0200

    bin/,dotconf.sh - Uber simplification

--- bin/,dotconf.sh
+++ bin/,dotconf.sh
@@ -6,17 +6,13 @@
 # default editor if the file already exists.
 #
 # Otherwise, create a new config file and it's symlink in $HOME.
-# 
-# Passing the -d flag as first argument will create a config directory.
 #
 # raphigaziano <r.gaziano@gmail.com>
 # Created: 05-20-2013
 #####################################################
 
-USAGE="`basename $0` [-d] <filename>[, <filename>...]"
+USAGE="`basename $0` <filename>[, <filename>...]"
 CONFDIR=~/.dotfiles
-
-### TODO: Better arg checking ###
 
 # Check args
 if [ $# -lt 1  ]
@@ -25,32 +21,17 @@ then
     exit 1
 fi
 
-# Naive flag detection
-if [[ $1 == -d  ]]; then
-    diropt=1
-    shift
-fi
-
-for filename in $*
-do
-    path=$CONFDIR/$filename
-    # Create files in ~/.dotfiles along with symlinks in $HOME
-    if [ ! -e $path ]; then
-        echo "create $path"
-        if [ -z $diropt ]; then     # Regular file
-            touch $path
-        else                        # Directory
-            mkdir $path             
-        fi
-        echo "new link ~/.${filename} -> ${path}"
-        ln -s $path ~/.$filename
-    fi
+files=()
+for filename in $*; do
+    files+=("$CONFDIR/$filename")
 done
 
-# Open files in system's default editor
-for filname in $*
-do
-    if [ -f $path ]; then
-        $EDITOR $CONFDIR/$filename
+$EDITOR ${files[@]}
+
+for f in $*; do
+    path=$CONFDIR/$filename
+    if [[ -e $path && ! -e $HOME/.$f ]]; then
+        echo "new link $HOME/.${f} -> ${path}"
+        ln -s $path $HOME/.$f
     fi
 done
