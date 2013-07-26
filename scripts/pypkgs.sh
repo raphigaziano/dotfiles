5df6d8d93b2cfc2ef6359a7548c9e57861b6206b
raphigaziano <r.gaziano@gmail.com>
2013-07-26 12:17:36 +0200

    better root/noroot handling in pypkgs - untested

--- scripts/pypkgs.sh
+++ scripts/pypkgs.sh
@@ -11,25 +11,42 @@
 #
 ##############################################################################
 
-# Do NOT run as root
-# TODO: warn user if so
-
-# Check if user is root
-# if [[ $EUID -ne 0 ]]; then
-    # echo "This script must be run as root"
-    # exit 1
-# fi
-
 CONFDIR=~/.dotfiles
 BURRITOURL=https://raw.github.com/brainsik/virtualenv-burrito/master/virtualenv-burrito.sh
 
+# Store root test for reuse
+root="[ $EUID -eq 0 ]"
+
+# Check if the script is run as root, and warn the user if so
+# warn and prompt him to decide if we keep going or abort.
+if $root; then
+    echo "You've ran pypkgs as the system's super user."
+    echo "This will bypass the virtualenv installation and install the packages"
+    echo "globally, which might not be what you want to do."
+    read -p "Keep going anyway [y/N] ? " answer
+    case $answer in 
+        [yY]* )
+            ;; # No-op, just keep going
+        * )
+            exit 0;;
+    esac
+fi
+
 # First get virtualenv & virtualenvwrapper, 
 # using venv-burrito to simplify the process
-curl -s $BURRITOURL | $SHELL
+if ! $root; then
+    echo "Installing venv-burrito..."
+    curl -s $BURRITOURL | $SHELL
 
-# Create a "master" virtual env ?
-# source $VENVBURRITO/startup.sh
+    # Create a "master" virtual env ?
+    # source $VENVBURRITO/startup.sh
+else
+    echo "Bypassing venv-burrito installation."
+fi
 
 # Install packages from pip-requirements.txt
 # (pip should be installed already)
-pip install --user -r $CONFDIR/scripts/pip-requirements.txt
+
+pipfile=$CONFDIR/scripts/pip-requirements.txt
+echo "Installing python packages listed in $pipfile..."
+pip install --user -r 
