fea71179edd12f35873a215b3efb1d2c8b0df64c
rga <r.gaziano@gmail.com>
2025-06-24 00:42:30 +0200

    vim and tmux integration

--- nvim/lua/raphi/lazy.lua
+++ nvim/lua/raphi/lazy.lua
@@ -27,6 +27,26 @@ require("lazy").setup({
     -- Colorscheme
 	{'rose-pine/neovim', name = 'rose-pine' },
 
+    -- tmux integration
+    {
+      "christoomey/vim-tmux-navigator",
+      cmd = {
+        "TmuxNavigateLeft",
+        "TmuxNavigateDown",
+        "TmuxNavigateUp",
+        "TmuxNavigateRight",
+        "TmuxNavigatePrevious",
+        "TmuxNavigatorProcessList",
+      },
+      keys = {
+        { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+        { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+        { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+        { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+        { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+      },
+    },
+
 	--- LSP config
 	{'williamboman/mason.nvim'},
 	{'williamboman/mason-lspconfig.nvim'},
